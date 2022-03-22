2c691144642147500fa9ed9f76eb4bba8a49fa1a
Vincent Jeannin <vincent.jeannin@he-arc.ch>
2022-03-22 22:36:49 +0100

    Update Rapport.txt and format files

--- Labo_1/script.js
+++ Labo_1/script.js
@@ -1,8 +1,7 @@
 /**
  * @brief Run all unit tests
  */
-function runAllTests()
-{
+function runAllTests() {
     // BINARY SUBTRACTION
     testBinarySubtraction1();
     testBinarySubtraction2();
@@ -67,7 +66,7 @@ function submit_addition() {
     $('binary_decode_addition').innerText = `Code float après décodage : ${float_code}`;
 }
 
-function submit_subtraction(){
+function submit_subtraction() {
     const num1 = parseFloat($("num1_sub").value);
     const num2 = parseFloat($("num2_sub").value);
     const nbBits = parseInt($("b_value").value);
@@ -85,7 +84,7 @@ function submit_subtraction(){
     $('binary_decode_subtraction').innerText = `Code float après décodage : ${float_code}`;
 }
 
-function float_subtraction(num1, num2, nbBits){
+function float_subtraction(num1, num2, nbBits) {
     const e_length = exponent_size(nbBits);
 
     const target_abs1 = Math.abs(num1);
@@ -102,14 +101,14 @@ function float_subtraction(num1, num2, nbBits){
     let mantis_array2 = getMantis(num2, nbBits, e2, e_length); // Get mantis of number 2
 
     // NORMALIZE MANTIS 1
-    while(mantis_array1.includes(1) && mantis_array1[0] == 0){
+    while (mantis_array1.includes(1) && mantis_array1[0] == 0) {
         mantis_array1.shift();
         mantis_array1.push(0);
         e1 -= 1;
     }
 
     // NORMALIZE MANTIS 2
-    while(mantis_array2.includes(1) && mantis_array2[0] == 0){
+    while (mantis_array2.includes(1) && mantis_array2[0] == 0) {
         mantis_array2.shift();
         mantis_array2.push(0);
         e2 -= 1;
@@ -118,12 +117,12 @@ function float_subtraction(num1, num2, nbBits){
     mantis_array1.unshift(1); // Add hidden bit (1)
     mantis_array2.unshift(1); // Add hidden bit (1)
 
-    if(e1 > e2){
+    if (e1 > e2) {
         maxExponent = e1;
 
         // Shift mantis of number2 by exponent_diff
         mantis_array2 = shiftMantis(mantis_array2, exponent_diff);
-    }else{
+    } else {
         maxExponent = e2;
 
         // Shift mantis of number1 by exponent_diff
@@ -132,26 +131,26 @@ function float_subtraction(num1, num2, nbBits){
 
     const sign = (num1 - num2) < 0 ? 1 : 0;
     let mantisRes_array = subBinaryNumbers(mantis_array1, mantis_array2);
-    
+
     // NORMALIZE RESULTING MANTIS
-    while(mantisRes_array.includes(1) && mantisRes_array[0] == 0){
+    while (mantisRes_array.includes(1) && mantisRes_array[0] == 0) {
         mantisRes_array.shift();
         mantisRes_array.push(0);
         maxExponent -= 1;
     }
 
     mantisRes_array.shift(); // Remove hidden bit
-    
+
     const d = Math.pow(2, e_length - 1) - 2;
     const exponent_value = maxExponent + d;
 
     return [sign, ...intToBinary(exponent_value, e_length), ...mantisRes_array];
 }
 
-function float_addition(num1, num2, nbBits){
+function float_addition(num1, num2, nbBits) {
     // ZEROS
-    if(num1 == 0) return encode_to_float(num2, nbBits);
-    if(num2 == 0) return encode_to_float(num1, nbBits);
+    if (num1 == 0) return encode_to_float(num2, nbBits);
+    if (num2 == 0) return encode_to_float(num1, nbBits);
 
     const e_length = exponent_size(nbBits);
 
@@ -171,12 +170,12 @@ function float_addition(num1, num2, nbBits){
     mantis_array1.unshift(1); // Add hidden bit (1)
     mantis_array2.unshift(1); // Add hidden bit (1)
 
-    if(e1 > e2){
+    if (e1 > e2) {
         maxExponent = e1;
 
         // Shift mantis of number2 by exponent_diff
         mantis_array2 = shiftMantis(mantis_array2, exponent_diff);
-    }else{
+    } else {
         maxExponent = e2;
 
         // Shift mantis of number1 by exponent_diff
@@ -186,62 +185,62 @@ function float_addition(num1, num2, nbBits){
     let sign = 0;
     let mantisRes_array;
 
-    if(num1 > 0 && num2 > 0){ // + +
+    if (num1 > 0 && num2 > 0) { // + +
         sign = 0;
         mantisRes_array = addBinaryNumbers(mantis_array1, mantis_array2);
     }
-    else if(num1 < 0 && num2 < 0){ // - -
+    else if (num1 < 0 && num2 < 0) { // - -
         sign = 1;
         mantisRes_array = addBinaryNumbers(mantis_array1, mantis_array2);
     }
-    else{ // - + OR + -
+    else { // - + OR + -
         return float_subtraction(num1, num2, nbBits);
     }
 
     // OVERFLOW
     let overflowDec = Math.abs(mantisRes_array.length - mantis_array1.length);
-    if(overflowDec > 0){
-        for(let i = 0; i < overflowDec; i++) mantisRes_array.pop(); // Remove weaker bit
+    if (overflowDec > 0) {
+        for (let i = 0; i < overflowDec; i++) mantisRes_array.pop(); // Remove weaker bit
         maxExponent += overflowDec; // Increase exponent
     }
 
     // NORMALIZE MANTIS
-    while(mantisRes_array.includes(1) && mantisRes_array[0] == 0){
+    while (mantisRes_array.includes(1) && mantisRes_array[0] == 0) {
         mantisRes_array.shift();
         mantisRes_array.push(0);
         maxExponent -= 1;
     }
 
     mantisRes_array.shift(); // Remove hidden bit
-    
+
     const d = Math.pow(2, e_length - 1) - 2;
     const exponent_value = maxExponent + d;
 
     return [sign, ...intToBinary(exponent_value, e_length), ...mantisRes_array];
 }
 
-function subBinaryNumbers(num1, num2){
+function subBinaryNumbers(num1, num2) {
     let res = [];
     let borrow = 0;
 
-    for(let i = num1.length - 1; i >= 0 ; i--){
+    for (let i = num1.length - 1; i >= 0; i--) {
         // If borrow == 1, that means that the current bit of the top number has been borrowed
         // So we have to set it to 0 if the current bit == 1
-        if(borrow == 1 && num1[i] == 1){
+        if (borrow == 1 && num1[i] == 1) {
             num1[i] = 0;
             borrow = 0;
         }
 
-        if(num1[i] == 0 && num2[i] == 1){ // BORROW CASE
+        if (num1[i] == 0 && num2[i] == 1) { // BORROW CASE
             res.unshift(1);
             borrow = 1;
         }
-        else{
+        else {
             res.unshift(Math.abs(num1[i] - num2[i]));
         }
     }
 
-    if(borrow == 1) res[0] = 0;
+    if (borrow == 1) res[0] = 0;
 
     return res;
 }
@@ -253,19 +252,19 @@ function subBinaryNumbers(num1, num2){
  * @param {Array<Number>} num2 The second number
  * @returns {Array<Number>} The result of the addition
  */
-function addBinaryNumbers(num1, num2){
+function addBinaryNumbers(num1, num2) {
     let resBinary = [];
     let carry = 0;
-    for(let i = num1.length - 1; i >= 0; i--){
-        if(num1[i] == 1 && num2[i] == 1){
-            if(carry == 1) resBinary.push(1);
+    for (let i = num1.length - 1; i >= 0; i--) {
+        if (num1[i] == 1 && num2[i] == 1) {
+            if (carry == 1) resBinary.push(1);
             else resBinary.push(0);
             carry = 1;
         }
-        else{
-            if(carry == 0) resBinary.push(num1[i] || num2[i]);
-            else{
-                if((num1[i] || num2[i]) == 1) resBinary.push(0);
+        else {
+            if (carry == 0) resBinary.push(num1[i] || num2[i]);
+            else {
+                if ((num1[i] || num2[i]) == 1) resBinary.push(0);
                 else {
                     resBinary.push(1);
                     carry = 0;
@@ -273,7 +272,7 @@ function addBinaryNumbers(num1, num2){
             }
         }
     }
-    if(carry == 1) resBinary.push(1);
+    if (carry == 1) resBinary.push(1);
     return resBinary.reverse();
 }
 
@@ -284,8 +283,8 @@ function addBinaryNumbers(num1, num2){
  * @param {Number} nbShifts The number of shifts needed
  * @returns {Array<Number>} The mantis shifter
  */
-function shiftMantis(array, nbShifts){
-    for(let i = 0; i < nbShifts; i++){
+function shiftMantis(array, nbShifts) {
+    for (let i = 0; i < nbShifts; i++) {
         array.pop();
         array.unshift(0);
     }
@@ -326,7 +325,7 @@ function encode_to_float(target, nbBits) {
  * @param {*} exponent_length Length of the exponent
  * @returns {Array<Number>} Binary array
  */
-function getMantis(target, nbBits, exponent, exponent_length){
+function getMantis(target, nbBits, exponent, exponent_length) {
     const n = nbBits - 1 - exponent_length;
     const mantis_array = [];
     const target_abs = Math.abs(target);
@@ -469,12 +468,12 @@ function exponent_size(size) {
  * @param {Array} arr2 The second array
  * @returns {Boolean}
  */
- function compareArray(arr1, arr2) {
+function compareArray(arr1, arr2) {
     // First, test numbers length
-    if(arr1.length != arr2.length) return false;
+    if (arr1.length != arr2.length) return false;
 
-    for(let i = 0; i < arr1.length; i++){
-        if(arr1[i] != arr2[i]) return false;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] != arr2[i]) return false;
     }
 
     return true; // Array are the same
@@ -484,7 +483,7 @@ function exponent_size(size) {
 *   UNIT TESTS   *
 *****************/
 
-function testBinarySubtraction1(){
+function testBinarySubtraction1() {
     let num1 = [1, 1, 0];
     let num2 = [1, 0, 1];
 
@@ -494,7 +493,7 @@ function testBinarySubtraction1(){
     console.assert(compareArray(resTheorique, resEmpirique), "Binary subtraction doesn't work. \nExpected result : " + resTheorique + " \nReal result : " + resEmpirique);
 }
 
-function testBinarySubtraction2(){
+function testBinarySubtraction2() {
     let num1 = [1, 0, 1, 1, 0]; // = 22
     let num2 = [0, 1, 1, 0, 1]; // = 13
 
@@ -504,7 +503,7 @@ function testBinarySubtraction2(){
     console.assert(compareArray(resTheorique, resEmpirique), "Binary subtraction doesn't work. \nExpected result : " + resTheorique + " \nReal result : " + resEmpirique);
 }
 
-function testBinarySubtraction3(){
+function testBinarySubtraction3() {
     let num1 = [1, 0, 0, 1]; // = 9
     let num2 = [0, 1, 0, 1]; // = 5
 
@@ -514,7 +513,7 @@ function testBinarySubtraction3(){
     console.assert(compareArray(resTheorique, resEmpirique), "Binary subtraction doesn't work. \nExpected result : " + resTheorique + " \nReal result : " + resEmpirique);
 }
 
-function testBinarySubtraction4(){
+function testBinarySubtraction4() {
     let num1 = [0, 1, 0, 1]; // = 5
     let num2 = [1, 0, 0, 1]; // = 9
 
@@ -524,8 +523,7 @@ function testBinarySubtraction4(){
     console.assert(compareArray(resTheorique, resEmpirique), "Binary subtraction doesn't work. \nExpected result : " + resTheorique + " \nReal result : " + resEmpirique);
 }
 
-function testMantisShifting()
-{
+function testMantisShifting() {
     const initialMantis = [1, 0, 1, 1, 0];
     const nbShift = 3;
 
@@ -545,7 +543,7 @@ function testBinaryAddition() {
     console.assert(compareArray(resTheorique, resEmpirique), "Binary addition doesn't work. \nExpected result : " + resTheorique + " \nReal result : " + resEmpirique);
 }
 
-function testFloatAddition1(){
+function testFloatAddition1() {
     let num1 = 13;
     let num2 = 26;
     let nbBits = 16;
@@ -556,7 +554,7 @@ function testFloatAddition1(){
     console.assert(resEmpirique == resTheorique, "Float addition doesn't work. \nExpected result : " + resTheorique + " \nReal result : " + resEmpirique);
 }
 
-function testFloatAddition2(){
+function testFloatAddition2() {
     let num1 = -1;
     let num2 = -23;
     let nbBits = 75;
@@ -567,7 +565,7 @@ function testFloatAddition2(){
     console.assert(resEmpirique == resTheorique, "Float addition doesn't work. \nExpected result : " + resTheorique + " \nReal result : " + resEmpirique);
 }
 
-function testFloatAddition3(){
+function testFloatAddition3() {
     let num1 = 45;
     let num2 = 23;
     let nbBits = 75;
@@ -578,7 +576,7 @@ function testFloatAddition3(){
     console.assert(resEmpirique == resTheorique, "Float addition (subtraction) doesn't work. \nExpected result : " + resTheorique + " \nReal result : " + resEmpirique);
 }
 
-function testFloatSubtraction1(){
+function testFloatSubtraction1() {
     let num1 = 12;
     let num2 = 19;
     let nbBits = 75;
@@ -589,7 +587,7 @@ function testFloatSubtraction1(){
     console.assert(resEmpirique == resTheorique, "Float subtraction doesn't work. \nExpected result : " + resTheorique + " \nReal result : " + resEmpirique);
 }
 
-function testFloatSubtraction2(){
+function testFloatSubtraction2() {
     let num1 = 28;
     let num2 = 16;
     let nbBits = 75;
@@ -600,7 +598,7 @@ function testFloatSubtraction2(){
     console.assert(resEmpirique == resTheorique, "Float subtraction doesn't work. \nExpected result : " + resTheorique + " \nReal result : " + resEmpirique);
 }
 
-function testFloatSubtraction3(){
+function testFloatSubtraction3() {
     let num1 = 29;
     let num2 = 29;
     let nbBits = 75;
