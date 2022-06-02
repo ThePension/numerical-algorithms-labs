f1228876331f04b27c0099453869a6ef43657b1d
Vuilliomenet Théo <theo.vuilliom@he-arc.ch>
2022-06-02 11:45:31 +0200

    Add group information

--- Projet/sketch.js
+++ Projet/sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-  createCanvas(400, 400);
+    // Create the canevas
+    createCanvas(windowWidth, 700);
+    background(200);
+
+    // Draw a splitter line
+    translate(width / 2, 0);
+    stroke(0);
+    line(0, 0, 0, height);
 }
 
 function draw() {
-  background(220);
+    // TODO
 }
