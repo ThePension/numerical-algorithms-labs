ad00087115c61d5f27c9b40c1dbe92edeb147e94
Vuilliomenet Théo <theo.vuilliom@he-arc.ch>
2022-06-02 17:30:12 +0200

    Make springs draggable

--- Projet/sketch.js
+++ Projet/sketch.js
@@ -17,20 +17,29 @@ let square_side = 20;
 let start_x;
 let start_y;
 
+// Mouse handling variables
+let over_spring1 = false;
+let over_spring2 = false;
+let locked_spring1 = false;
+let locked_spring2 = false;
+let xOffset_spring1 = 0.0;
+let yOffset_spring1 = 0.0;
+let xOffset_spring2 = 0.0;
+let yOffset_spring2 = 0.0;
+
 /* ==================================================== *\
 |*                        SETUP                         *|
 \* ==================================================== */
 
 function setup() {
     // Create the canevas
-    let canevasHeight = 700;
-    createCanvas(windowWidth, canevasHeight);
+    createCanvas(windowWidth, 700);
 
     // Globals initialization
     spring_round_color = color(204, 102, 0);
     spring_line_color = color(45, 197, 244);
-    start_x = (3 * windowWidth / 4) - square_side / 2;
-    start_y = canevasHeight / 4;
+    start_x = (3 * width / 4) - square_side / 2;
+    start_y = height / 4;
 }
 
 /* ==================================================== *\
@@ -49,6 +58,9 @@ function draw() {
     // Draw the base square
     rect(start_x, start_y, square_side, square_side);
 
+    // Test if the mouse is over one of the springs
+    handleMouseOverSprings();
+
     // Animation test
     //spring_1_x += 0.2;
     //spring_1_y -= 0.5;
@@ -60,6 +72,11 @@ function draw() {
 
     // Draw the springs from the vectors
     drawSprings(vector_base_square, vector_spring_1, vector_spring_2);
+
+    // Draw White rectangle where the graph will be
+    stroke(255);
+    fill(255);
+    rect(0, 0, (width / 2) - 2, height);
 }
 
 function drawSprings(vector0, vector1, vector2) {
@@ -81,4 +98,76 @@ function drawSprings(vector0, vector1, vector2) {
     stroke(spring_round_color);
     fill(spring_round_color);
     circle(0, 0, 2 * square_side);
+
+    // Go back to the top left corner of the canevas
+    translate(-vector2.x, -vector2.y);
+    translate(-vector1.x, -vector1.y);
+    translate(-vector0.x, -vector0.y);
+}
+
+/* ==================================================== *\
+|*                 DRAG AND DROP SPRINGS                *|
+\* ==================================================== */
+
+function handleMouseOverSprings() {
+    // TODO : Improve over box
+    // For now the over box are simple uncentered square
+
+    if (
+        mouseX > spring_1_x + start_x - 2 * square_side &&
+        mouseX < spring_1_x + start_x + 2 * square_side &&
+        mouseY > spring_1_y + start_y - 2 * square_side &&
+        mouseY < spring_1_y + start_y + 2 * square_side
+    ) {
+        over_spring1 = true;
+    } else {
+        over_spring1 = false;
+    }
+
+    if (
+        mouseX > spring_1_x + spring_2_x + start_x - 2 * square_side &&
+        mouseX < spring_1_x + spring_2_x + start_x + 2 * square_side &&
+        mouseY > spring_1_y + spring_2_y + start_y - 2 * square_side &&
+        mouseY < spring_1_y + spring_2_y + start_y + 2 * square_side
+    ) {
+        over_spring2 = true;
+    } else {
+        over_spring2 = false;
+    }
+}
+
+function mousePressed() {
+    if (over_spring1) {
+        locked_spring1 = true;
+    } else {
+        locked_spring1 = false;
+    }
+
+    if (over_spring2) {
+        locked_spring2 = true;
+    } else {
+        locked_spring2 = false;
+    }
+
+    xOffset_spring1 = mouseX - spring_1_x;
+    yOffset_spring1 = mouseY - spring_1_y;
+    xOffset_spring2 = mouseX - spring_2_x;
+    yOffset_spring2 = mouseY - spring_2_y;
+}
+
+function mouseDragged() {
+    if (locked_spring1) {
+        spring_1_x = mouseX - xOffset_spring1;
+        spring_1_y = mouseY - yOffset_spring1;
+    }
+
+    if (locked_spring2) {
+        spring_2_x = mouseX - xOffset_spring2;
+        spring_2_y = mouseY - yOffset_spring2;
+    }
+}
+
+function mouseReleased() {
+    locked_spring1 = false;
+    locked_spring2 = false;
 }
