fa3584efc19f17ef0a54824e4097dd45e16d42d9
Lucas Gosteli <lucas.gosteli@he-arc.ch>
2022-06-08 22:16:43 +0200

    small improvement on the controls
    Update controls.js and index.html

--- Projet/Double_2D_Spring/controls.js
+++ Projet/Double_2D_Spring/controls.js
@@ -34,27 +34,48 @@ function simulation_reset() {
 
 function simulation_pause() {
     ispaused = !ispaused;
-    button.innerHTML = ispaused ? 'unpause' : 'pause';
+    sumulation_pause.innerHTML = ispaused ? 'unpause' : 'pause';
 }
 
 function simulation_gravity() {
     g = +(gravity_input.value);
-    gravity_value.value = parseFloat(gravity_input.value).toFixed(2).padStart(5,'0');
+    gravity_value.value = parseFloat(gravity_input.value).toFixed(2).padStart(5, '0');
 }
 
 function simulation_damping() {
     b = 3 * +(damping_input.value);
-    damping_value.value  = parseFloat(damping_input.value).toFixed(2).padStart(3,'0')
+    damping_value.value = parseFloat(damping_input.value).toFixed(2).padStart(3, '0')
 }
 
 function springs_rest_length() {
+    if (+(rest_length_input.value) < 1) {
+        rest_length_input.value = 1;
+    }
+
     R = +(rest_length_input.value);
 }
 
+function springs_constant() {
+    if (+(spring_constant_input.value) < 1) {
+        spring_constant_input.value = 1;
+    }
+
+    k = +(spring_constant_input.value);
+}
+
 function springs_mass1() {
+    if (+(mass1_input.value) < 1) {
+        mass1_input.value = 1;
+    }
+
     m1 = +(mass1_input.value);
 }
 
 function springs_mass2() {
+    if (+(mass2_input.value) < 1) {
+
+        mass2_input.value = 1;
+    }
+    
     m2 = +(mass2_input.value);
 }
