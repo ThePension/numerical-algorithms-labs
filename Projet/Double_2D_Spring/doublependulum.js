c7e0bea91abddfa916e46f4166131c9a1fb30afe
nicolas.aubert1 <nicolas.aubert1@hes-so.ch>
2022-06-15 16:18:28 +0200

    Adding comments

--- Projet/Double_2D_Spring/doublependulum.js
+++ Projet/Double_2D_Spring/doublependulum.js
@@ -1,44 +1,60 @@
+/**
+ * @class DoublePendulum
+ */
 class DoublePendulum
 {
-    constructor(dec, color)
+    /**
+     * Construct a double pendulum object
+     * @param {float} offset Offset from the origin (for the second ball)
+     * @param {Color} color Color of the pendulum
+     */
+    constructor(offset, color)
     {
+        // Radius in pixel
         this.r1 = 200;
         this.r2 = 200;
+
+        // Angle (rad)
         this.a1 = 3;
-        this.a2 = 3 + dec;
+        this.a2 = 3 + offset;
+
+        // Angular velocity
         this.a1_v = 0;
         this.a2_v = 0;
+
+        // Angular acceleration
         this.a1_a = 0;
         this.a2_a = 0;
 
+        // Position of the first ball
         this.x1;
         this.y1;
         
+        // Position of the second ball
         this.x2;
         this.y2;
 
+        // Color
         this.color = color;
 
-        this.square_side = 20;
-
+        // Calculate position of the first mass
         this.x1 = this.r1 * sin(this.a1);
         this.y1 = this.r1 * cos(this.a1);
 
+        // Calculate position of the second mass
         this.x2 = this.x1 + this.r2 * sin(this.a2);
         this.y2 = this.y1 + this.r2 * cos(this.a2);
 
-        this.isMoving = false;
+        this.dragging1 = false; // Is the first ball being dragged ?
+        this.dragging2 = false; // Is the second ball being dragged ?
 
-        this.dragging1 = false; // Is the object being dragged?
-        this.dragging2 = false;
-
-        this.offsetA1 = 0;
-        this.offsetA2 = 0;
-
-        this.prevOffsetA1 = 0;
-        this.prevOffsetA2 = 0;
+        this.previous_a1 = 0;
+        this.previous_a2 = 0;
     }
 
+    /**
+     * Display the pendulum
+     */
     show()
     {
         stroke(this.color);
@@ -52,64 +68,79 @@ class DoublePendulum
         line(this.x1, this.y1, this.x2, this.y2);
 
         // Draw the first ball
-        circle(this.x1, this.y1, 2 * this.square_side);
+        circle(this.x1, this.y1, 2 * square_side);
 
         // Draw the second ball
-        circle(this.x2, this.y2, 2 * this.square_side);
+        circle(this.x2, this.y2, 2 * square_side);
 
         // Go back to the top left corner of the canevas
         resetMatrix();
     }
 
+    /**
+     * Update the position of the two balls
+     */
     update()
     {
+        // If no ball is being dragged
         if (!this.dragging1 && !this.dragging2)
         {
+            // Calculate the angular acceleration of the two mass
             this.computeAngularAccelerationForM1();
             this.computeAngularAccelerationForM2();
 
+            // Apply these accelerations to the angular velocity
             this.a1_v += this.a1_a / 15.0;
             this.a2_v += this.a2_a / 15.0;
 
+            // Apply the velocity to the position 
             this.a1 += this.a1_v;
             this.a2 += this.a2_v;
         }
         else
         {
+            // If the first ball is being dragger
             if(this.dragging1)
             {
+                // Get the relative position of the mouse (from the center of the screen)
                 let relativeMouseX = mouseX - width / 2;
                 let relativeMouseY = (height / 2) - mouseY;
 
-                this.offsetA1 = atan2(relativeMouseY, relativeMouseX) + PI/2;
+                // Calculate the angle based on the mouse relative position
+                this.a1 = atan2(relativeMouseY, relativeMouseX) + PI/2;
 
-                this.a1 = this.offsetA1;
-                this.a1_v = this.offsetA1 - this.prevOffsetA1;
+                // Set the velocity of the first ball based the current angle and the previous one
+                this.a1_v = this.a1 - this.previous_a1;
             }
-            else if(this.dragging2)
+            else if(this.dragging2) // If the second ball is being dragged
             {
+                // Get the relative position of the mouse (from the position of the first ball)
                 let relativeMouseXForM2 = mouseX - (width / 2 + this.x1);
                 let relativeMouseYForM2 = (height / 2 + this.y1) - mouseY;
+ 
+                // Calculate the angle based on the mouse relative position
+                this.a2 = -atan2(relativeMouseXForM2, relativeMouseYForM2) + PI;
 
-                this.offsetA2 = -atan2(relativeMouseXForM2, relativeMouseYForM2) + PI;
-                this.a2 = this.offsetA2;
+                // Set the velocity of the second ball based on the current angle and the previous one
+                this.a2_v = this.a2 - this.previous_a2;
 
-                this.a2_v = this.offsetA2 - this.prevOffsetA2;
-
+                // Block the angular acceleration of the second ball
                 this.a2_a = 0;
+
+                // Block the velocity of the first ball
                 this.a1_v = 0;
             }
 
             // Get the previous angle
-            this.prevOffsetA1 = this.offsetA1;
-            this.prevOffsetA2 = this.offsetA2;
+            this.previous_a1 = this.a1;
+            this.previous_a2 = this.a2;
         }
 
-        // Update position of the first mass
+        // Calculate position of the first mass
         this.x1 = this.r1 * sin(this.a1);
         this.y1 = this.r1 * cos(this.a1);
 
-        // Update position of the second mass
+        // Calculate position of the second mass
         this.x2 = this.x1 + this.r2 * sin(this.a2);
         this.y2 = this.y1 + this.r2 * cos(this.a2);
 
@@ -118,18 +149,27 @@ class DoublePendulum
         this.a2_v *= 0.999;
     }
 
+    /**
+     * Mouse Pressed event
+     */
     pressed()
     {
+        // If the mouse is over the first ball
         if (this.overMass1())
         {
+            // Start dragging the first ball
             this.dragging1 = true;
         }
-        else if(this.overMass2())
+        else if(this.overMass2()) // If the mouse is over the second ball
         {
+            // Start dragging the second ball
             this.dragging2 = true;
         }
     }
 
+    /**
+     * Mouse Released event
+     */
     released()
     {
         // Quit dragging
@@ -137,22 +177,33 @@ class DoublePendulum
         this.dragging2 = false;
     }
 
+    /**
+     * Check if the mouse is over the first ball
+     * @returns {bool}
+     */
     overMass1()
     {
         let relativeMouseX = mouseX - width / 2;
         let relativeMouseY = mouseY - height / 2;
         // Is mouse over object
-        return relativeMouseX > this.x1 - this.square_side * 2 && relativeMouseX < this.x1 + this.square_side * 2 && relativeMouseY > this.y1 - this.square_side * 2 && relativeMouseY < this.y1 + this.square_side * 2;
+        return relativeMouseX > this.x1 - square_side * 2 && relativeMouseX < this.x1 + square_side * 2 && relativeMouseY > this.y1 - square_side * 2 && relativeMouseY < this.y1 + square_side * 2;
     }
 
+    /**
+     * Check if the mouse is over the second ball
+     * @returns {bool}
+     */
     overMass2()
     {
         let relativeMouseX = mouseX - width / 2;
         let relativeMouseY = mouseY - height / 2;
         // Is mouse over object
-        return relativeMouseX > this.x2 - this.square_side * 2 && relativeMouseX < this.x2 + this.square_side * 2 && relativeMouseY > this.y2 - this.square_side * 2 && relativeMouseY < this.y2 + this.square_side * 2;           
+        return relativeMouseX > this.x2 - square_side * 2 && relativeMouseX < this.x2 + square_side * 2 && relativeMouseY > this.y2 - square_side * 2 && relativeMouseY < this.y2 + square_side * 2;           
     }
 
+    /**
+     * Calculate the acceleration of the first ball
+     */
     computeAngularAccelerationForM1()
     {
         let num1 = -g * (2 * m1 + m2) * sin(this.a1);
@@ -163,6 +214,9 @@ class DoublePendulum
         this.a1_a = (num1 + num2 + num3 * num4) / den;
     }
 
+    /**
+     * Calculate the acceleration of the second ball
+     */
     computeAngularAccelerationForM2()
     {
         let num1 = 2 * sin(this.a1 - this.a2);
--- Projet/Double_2D_Spring/sketch.js
+++ Projet/Double_2D_Spring/sketch.js
@@ -10,13 +10,22 @@
 |*                       GLOBALS                        *|
 \* ==================================================== */
 
-pendulums_number = 5;
-g = 9.81;
-m1 = 10;
-m2 = 10;
+// Number of pendulums
+let pendulums_number = 5;
 
+// Gravity value
+let g = 9.81;
+
+// Mass of the first ball
+let m1 = 10;
+
+// Mass of the second ball
+let m2 = 10;
+
+// Array that will contain the double pendulums
 pendulums = [];
 
+// Size of a ball
 let square_side = 20;
 
 /* ==================================================== *\
@@ -25,13 +34,17 @@ let square_side = 20;
 
 function setup()
 {
+    // Create the pendulums, and add them the array
     for(let i = 0; i < pendulums_number; i++)
     {
         pendulums.push(new DoublePendulum(i / 100000, color(map(i, 0, pendulums_number, 0, 255), map(i, 0, pendulums_number, 0, 255), map(i, 0, pendulums_number, 0, 255))));
     }
+
     // Create the canevas
     let canvas = createCanvas(windowWidth, windowHeight - 50);
     canvas.parent('sketch');
+
+    // Set the framerate to 60
     frameRate(60);
 }
 
@@ -41,11 +54,13 @@ function setup()
 
 function draw()
 {
+    // Draw the background
     background(130, 130, 250);
     strokeWeight(2);
     stroke(0);
     fill(0);
 
+    // Update and Show all pendulums in the array
     for(let i = 0; i < pendulums.length; i++)
     {
         pendulums[i].update();
@@ -53,18 +68,28 @@ function draw()
     }
 }
 
+/**
+ * Mouse Pressed event
+ */
 function mousePressed() 
 {
+    // For all pendulums
     for(let i = 0; i < pendulums.length; i++)
     {
+        // Call the corresponding function (pressed)
         pendulums[i].pressed();
     }
 }
 
+/**
+ * Mouse Released event
+ */
 function mouseReleased() 
 {
+    // For all pendulums
     for(let i = 0; i < pendulums.length; i++)
     {
+        // Call the corresponding function (released)
         pendulums[i].released();
     }
 }
